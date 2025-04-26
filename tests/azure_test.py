2e746af47e7761fdfa95c572256d90e745cc6cfd
LMY <137768603+DengDingDing@users.noreply.github.com>
2025-04-26 12:44:05 +0000

    azure

--- tests/azure_test.py
+++ tests/azure_test.py
@@ -1,10 +1,24 @@
+import os 
+os.environ['AZURE_SPEECH_KEY'] = 'E1lgRaKZjlQfl7WOQL7rRw9T37vA5rRJQ10vpawcGtsvsOqVZesBJQQJ99BDAC3pKaRXJ3w3AAAYACOGHL69' 
+os.environ['AZURE_SPEECH_REGION'] = 'eastasia'
 if __name__ == "__main__":
     import os
     from RealtimeTTS import TextToAudioStream, AzureEngine
+    
 
     def dummy_generator():
-        yield "Hey guys! These here are realtime spoken sentences based on local text synthesis. "
-        yield "With a local, neuronal, cloned voice. So every spoken sentence sounds unique."
+        yield "你好，欢迎使用Azure语音合成服务。"
+        yield "今天的天气真不错。"
+        yield "今天的天气真不错。"
+    
+    # text = '请介绍人工智能发展历史'
+    def genai_stream(apikey, text):
+        genai.configure(api_key=AIzaSyCR-J-KZD-UvzHc7oQfLHC9pTALhNFYU7s)
+        model = genai.GenerativeModel('gemini-2.0-flash')
+        response = model.generate_content(text, stream=True)
+        for chunk in response:
+            print(chunk.text, end='', flush=True)
+
 
     # for normal use with minimal logging:
     import os
@@ -13,6 +27,7 @@ if __name__ == "__main__":
         os.environ["AZURE_SPEECH_REGION"],
         audio_format="riff-48khz-16bit-mono-pcm"
     )
+    print("完成引擎设置")
 
     import string
     last_word = None
@@ -24,9 +39,10 @@ if __name__ == "__main__":
         print(f"{word.word}", end="", flush=True)
         last_word = word.word
 
-    engine.set_voice("Ava")
+    engine.set_voice("zh-CN-XiaoxiaoNeural")
+    print("voice设置完成")
 
-    stream = TextToAudioStream(engine, on_word=process_word)
+    stream = TextToAudioStream(engine, on_word=process_word,muted=True)
 
     print("Starting to play stream")
     stream.feed(dummy_generator()).play(output_wavfile = "output.wav")
